e0fd8620474256deb9e0bc155b8a28f1bcec081b
CalinCretu <calincretu1412@gmail.com>
2023-11-02 13:53:43 +0100

    ADD - axios_confirm

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
 console.log('prova')
+
+axios
+  .get('https://flynn.boolean.careers/exercises/api/random/mail')
+  .then((res) => {
+    console.log('risposta server')
+    console.log(res);
+    console.log(res.data);
+  })
