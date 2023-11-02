c6c5189f76f3773060b936c3cb1b8f72485551a4
CalinCretu <calincretu1412@gmail.com>
2023-11-02 14:08:36 +0100

    ADD - one_random_mail_print

--- js/app.js
+++ js/app.js
@@ -1,9 +1,36 @@
 console.log('prova')
 
-axios
-  .get('https://flynn.boolean.careers/exercises/api/random/mail')
-  .then((res) => {
-    console.log('risposta server')
-    console.log(res);
-    console.log(res.data);
-  })
+const { createApp } = Vue
+
+createApp({
+  data() {
+    return {
+      randomMail: '',
+    }
+  },
+  methods: {
+    fetchMail() {
+      axios
+        .get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then((res) => {
+          console.log(res.data.response)
+          const mail = res.data.response
+          this.randomMail = mail
+        })
+    }
+  },
+  created() {
+    this.fetchMail()
+  },
+}).mount('#app')
+
+
+
+
+// axios
+//   .get('https://flynn.boolean.careers/exercises/api/random/mail')
+//   .then((res) => {
+//     console.log('risposta server')
+//     console.log(res);
+//     console.log(res.data);
+//   })
